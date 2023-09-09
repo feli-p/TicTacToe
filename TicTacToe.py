03b8c6e0e59f5bbfa975a26ba855507c8fff3f05
feli-p <felipe-0019@hotmail.com>
2023-09-09 14:05:41 -0600

    Implementación de llenado de tablero

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,10 +1,18 @@
-# Juego de gato. Proyecto prueba.
-# Esto es una preuba
+"""
+Este proyecto es un juego de gato (tic tac toe).
+Está hecho para practicar programación en Python, por lo que contiene elementos innecesarios para el juego
+pero que se usaron para practicar programación con clases en este lenguaje.
+"""
+
+import random
 
 class Tablero:
     def __init__(self):
         self.jugadas = [0]*9
-        self.tablero = """
+        
+
+    def imprimeTablero(self):
+        tabla = """
         +---+---+---+
         | {} | {} | {} |
         +---+---+---+
@@ -13,9 +21,7 @@ class Tablero:
         | {} | {} | {} |
         +---+---+---+
         """
-
-    def imprimeTablero(self):
-        print(self.tablero.format(*self.jugadas).replace('0','-').replace('1', 'X').replace('-1', 'O'))
+        print(tabla.format(*self.jugadas).replace('0','-').replace('-1', 'O').replace('1', 'X'))
 
 
 class Jugador:
@@ -31,36 +37,105 @@ class Jugador:
 
 
 class JugadorHumano(Jugador):
-    #def __init__(self):
-        #super().__init__()
-    pass
+    def __init__(self):
+        super().__init__()
+    
+    def movimiento(self, tablero, turno):
+        bandera = True
+        while(bandera):
+
+            bandera2 = True
+            while(bandera2):
+                entradaUsuario = input("\tEscoge dónde poner tu figura:\t")
+                if entradaUsuario.isdigit():
+                    index = int(entradaUsuario)
+                    if index > 8:
+                        print("\t\tIngresa un número entre 0 y 8\n")
+                    else:
+                        bandera2 = False
+                else:
+                    print("\t\tIngresa un número entre 0 y 8\n")
+
+            if tablero.jugadas[index] == 0:
+                tablero.jugadas[index] = turno
+                bandera = False
+            else:
+                print("\t\tNúmero inválido, escoge de nuevo.\n")
+            
+
 
 class CPU(Jugador):
-    pass
+    def movimiento(self, tablero, turno):
+        bandera = True
+        while(bandera):
+            index = random.randint(0,8)
+            if tablero.jugadas[index] == 0:
+                tablero.jugadas[index] = turno
+                bandera = False
 
 
-class Gato():
+class Partida():
     def __init__(self):
-        self.turno = 0
+        self.tablero = Tablero()
+        self.ronda = 0
+        self.fin = False
+        self.jugadores = []
+
+    def crearJugadores(self):
+        print("Introduce el nombre de los jugadores. Si escribes CPU, el jugador correspondiente será la computadora.")
+        name1 = input("\tJugador 1:\n\t\t")
+        if name1 != 'CPU':
+            jugador1 = JugadorHumano()
+            jugador1.nombre = name1
+        else:
+            jugador1 = CPU()
+            jugador1.nombre = 'CPU 1'
+        jugador1.setID(1)
+
+        name2 = input("\tJugador 2:\n\t\t")
+        if name2 != 'CPU':
+            jugador2 = JugadorHumano()
+            jugador2.nombre = name2
+        else:
+            jugador2 = CPU()
+            jugador2.nombre = 'CPU 2'
+        jugador2.setID(-1)
+
+        self.jugadores.append(jugador1)
+        self.jugadores.append(jugador2)
+        print("\n")
     
+    def jugada(self):
+        print(f"Ronda {self.ronda+1}: ",end = '')
+        aux = self.ronda % 2
+        turno = self.jugadores[aux].id
+        print(f"Turno de {self.jugadores[aux].nombre}")
+        
+        self.jugadores[aux].movimiento(self.tablero, turno)
+        
+        self.tablero.imprimeTablero()
+        self.ronda += 1
 
+    def revisarFin(self):
+        a = input("¿Ya acabó? Pon 1 si sí \n")
+        if a == 1:
+            self.fin = True
+
+    def iniciaPartida(self):
+        self.crearJugadores()
+        while(not(self.fin) and self.ronda < 9):
+            self.jugada()
+        print("¡Gracias por jugar!")
+
+        
+        
 if __name__=='__main__':
-    print("Bienvenidos al juego de gato")
-    print("Hecho por: losa lucines gpt")
-    print("----------------------------")
+    print("""
+            Bienvenidos al juego de gato
+            Hecho por: losa lucines gpt
+        ------------------------------------
+          """)
 
-    tablero = Tablero()
-    tablero.imprimeTablero()
-    tablero.jugadas[0] = 1
-    tablero.imprimeTablero()
-
-    player1 = Jugador()
-    player2 = JugadorHumano()
-    player3 = CPU()
-    player2.setID(1)
-    player3.setID(-1)
-
-    player1.printID()
-    player2.printID()
-    player3.printID()
+    juego = Partida()
+    juego.iniciaPartida()
     
