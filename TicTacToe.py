762990f7fa8b671449373ed75b4eb328c20a199c
feli-p <felipe-0019@hotmail.com>
2023-09-08 20:45:56 -0600

    Funcionalidad Tablero

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,5 +1,32 @@
 import random
 
-print("Bienvenidos al juego de gato")
-print("----------------------------")
 
+class Tablero:
+    def __init__(self):
+        self.jugadas = [0]*9
+        self.tablero = """
+        +---+---+---+
+        | {} | {} | {} |
+        +---+---+---+
+        | {} | {} | {} |
+        +---+---+---+
+        | {} | {} | {} |
+        +---+---+---+
+        """
+
+    def imprimeTablero(self):
+        print(self.tablero.format(*self.jugadas).replace('0','-').replace('1', 'X').replace('-1', 'O'))
+
+
+
+
+
+if __name__=='__main__':
+    print("Bienvenidos al juego de gato")
+    print("Hecho por: losa lucines gpt")
+    print("----------------------------")
+
+    tablero = Tablero()
+    tablero.imprimeTablero()
+    tablero.jugadas[0] = 1
+    tablero.imprimeTablero()
