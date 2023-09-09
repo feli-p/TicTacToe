7fdb0631562faa920f1b54e456cdf0fbe3ebd529
feli-p <felipe-0019@hotmail.com>
2023-09-08 21:25:24 -0600

    Fin día 1

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,4 +1,4 @@
-import random
+# Juego de gato. Proyecto prueba.
 
 
 class Tablero:
@@ -18,8 +18,31 @@ class Tablero:
         print(self.tablero.format(*self.jugadas).replace('0','-').replace('1', 'X').replace('-1', 'O'))
 
 
+class Jugador:
+    def __init__(self):
+        self.nombre = ""
+        self.id = 0
+
+    def setID(self, newID):
+        self.id = newID
+
+    def printID(self):
+        print(self.id)
 
 
+class JugadorHumano(Jugador):
+    #def __init__(self):
+        #super().__init__()
+    pass
+
+class CPU(Jugador):
+    pass
+
+
+class Gato():
+    def __init__(self):
+        self.turno = 0
+    
 
 if __name__=='__main__':
     print("Bienvenidos al juego de gato")
@@ -30,3 +53,14 @@ if __name__=='__main__':
     tablero.imprimeTablero()
     tablero.jugadas[0] = 1
     tablero.imprimeTablero()
+
+    player1 = Jugador()
+    player2 = JugadorHumano()
+    player3 = CPU()
+    player2.setID(1)
+    player3.setID(-1)
+
+    player1.printID()
+    player2.printID()
+    player3.printID()
+    
